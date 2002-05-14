4b6a50a85e855505f69bfaa510839bf2124cbac4
Steve McKay <mckay@cpan.org>
2002-05-14 17:25:32 +0000

    Import of MCKAY/Digest-Elf-1.3 from CPAN.
    gitpan-cpan-distribution: Digest-Elf
    gitpan-cpan-version:      1.3
    gitpan-cpan-path:         MCKAY/Digest-Elf-1.3.tar.gz
    gitpan-cpan-author:       MCKAY
    gitpan-cpan-maturity:     released

---
+++ Elf.pm
@@ -0,0 +1,66 @@
+package Digest::Elf;
+
+use 5.006;
+use strict;
+use warnings;
+use vars qw( $VERSION );
+
+require Exporter;
+require DynaLoader;
+
+our @ISA = qw(Exporter DynaLoader);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use Digest::Elf ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	elf
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
+
+$VERSION = sprintf( "%s", q{$Id: Elf.pm,v 1.3 2002/05/15 23:08:19 steve Exp $} =~ /(\d+\.\d+)/ );
+
+bootstrap Digest::Elf $VERSION;
+
+# Preloaded methods go here.
+
+1;
+__END__
+
+=head1 NAME
+
+Digest::Elf - Perl extension for generating ElfHash values.
+
+=head1 SYNOPSIS
+
+  use Digest::Elf;
+  print Digest::Elf::elf( "foo" );
+
+=head1 DESCRIPTION
+
+Digest::Elf is an implimentation of the ElfHash algorithm.
+
+=head2 FUNCTIONS
+
+=item elf( VALUE )
+
+	Create a hash value based on the input value.
+
+=head1 AUTHOR
+
+Steve McKay, E<lt>steve@colgreen.comE<gt>
+
+=head1 SEE ALSO
+
+L<perl>.
+
+=cut
